402a67c261ccb493cdf90ec9a2b2b2df3fd76ca9
vatsks <iamksvats@gmail.com>
2021-06-23 02:03:05 +0530

    Added a volume seekbar functionality.

--- app/src/main/java/com/example/sounddemo/MainActivity.java
+++ app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.sounddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     public void start(View view){
 
-        mediaPlayer=MediaPlayer.create(this,R.raw.birdschirping);
+
         mediaPlayer.start();
 
     }
@@ -26,8 +30,36 @@ public void pause(View view){
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audioManager= (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxvol =audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentvol=audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
 
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.birdschirping);
+
+        SeekBar seekBar=findViewById(R.id.seekBarvolume);
+        seekBar.setMax(maxvol);
+        seekBar.setProgress(currentvol);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                Log.i("info",Integer.toString(progress));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 }
